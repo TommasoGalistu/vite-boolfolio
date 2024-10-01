06b76c6000a9182ea3bc19be9070af75def1d5fe
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-10-01 15:37:57 +0200

    feat: add data wit api request

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -5,14 +5,16 @@ export default {
   data() {
     return {
       posts: [],
+      categories: [],
+      types: [],
     };
   },
   methods: {
-    getApi() {
+    getApi(urlApi) {
       axios
-        .get("http://127.0.0.1:8000/api/posts")
+        .get(urlApi)
         .then((response) => {
-          console.log(response);
+          this.posts = response.data;
         })
         .catch((error) => {
           console.log(error);
@@ -20,12 +22,42 @@ export default {
     },
   },
   mounted() {
-    this.getApi();
+    this.getApi("http://127.0.0.1:8000/api/posts");
   },
 };
 </script>
 <template>
-  <h1 class="">PosterPage</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(post, index) in posts.data" :key="index">
+              <td>{{ post.id }}</td>
+              <td>{{ post.title }}</td>
+              <td>{{ post.category?.name ? post.category.name : "-" }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button
+          class="btn btn-primary"
+          v-for="(link, index) in posts.links"
+          :key="index"
+          v-html="link.label"
+          @click="getApi(link.url)"
+          :disabled="active"
+        ></button>
+      </div>
+      <div class="col-4">colonna dei tipi e categorie</div>
+    </div>
+  </div>
 </template>
 <style lang='scss' scoped>
 // @use 'path' as *;

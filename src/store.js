d00aca169cfb1524e8f8686fb35673a20f6b3589
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-10-01 16:39:20 +0200

    feat: add categories and type

---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  urlPath: "http://127.0.0.1:8000/api/",
+});
--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,9 +1,11 @@
 <script>
+import { store } from "../store";
 import axios from "axios";
 export default {
   name: "PosterPage",
   data() {
     return {
+      store,
       linkUrl: "",
       posts: [],
       categories: [],
@@ -11,21 +13,30 @@ export default {
     };
   },
   methods: {
-    getApi(urlApi) {
+    getApi(urlApi, type) {
+      let urlRequest = urlApi + type;
       axios
-        .get(urlApi)
+        .get(urlRequest)
         .then((response) => {
-          this.posts = response.data;
+          if (type === "posts") {
+            this.posts = response.data;
+          } else {
+            this[type] = response.data;
+          }
           // attivo il bottone delle pagine
           this.linkUrl = urlApi;
         })
         .catch((error) => {
-          console.log(error);
+          console.log("Errore: ", error);
         });
     },
   },
   mounted() {
-    this.getApi("http://127.0.0.1:8000/api/posts");
+    this.getApi(this.store.urlPath, "posts");
+    this.getApi(this.store.urlPath, "categories");
+    this.getApi(this.store.urlPath, "types");
+    // this.getApi("http://127.0.0.1:8000/api/categories");
+    // this.getApi("http://127.0.0.1:8000/api/types");
   },
 };
 </script>
@@ -64,7 +75,26 @@ export default {
           :class="{ active: link.url === linkUrl }"
         ></button>
       </div>
-      <div class="col-4">colonna dei tipi e categorie</div>
+      <div class="col-4 d-flex flex-column gap-5">
+        <div>
+          <button
+            class="btn btn-dark"
+            v-for="(category, index) in categories"
+            :key="index"
+          >
+            {{ category.name }}
+          </button>
+        </div>
+        <div>
+          <button
+            class="btn btn-light"
+            v-for="(type, index) in types"
+            :key="index"
+          >
+            {{ type.name }}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
